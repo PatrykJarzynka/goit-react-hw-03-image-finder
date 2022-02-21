c3433f20348b6f58f2d8c7bd56d0d8a47e8e0e86
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2022-02-21 17:56:06 +0100

    Poprawiono link

--- src/services/pixabay.js
+++ src/services/pixabay.js
@@ -2,8 +2,8 @@ const KEY = '24835588-34c67f39a9342d1bd89adf1b2';
 
 const fetchImages = name => {
   fetch(
-    `https://pixabay.com/api/?q=${name}&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`,
-  ).then(res => console.log(res));
+    `https://pixabay.com/api/?key=${KEY}&q=${name}&page=1&image_type=photo&orientation=horizontal&per_page=12`,
+  ).then(res => console.log(res.json()));
 };
 
 export { fetchImages };
