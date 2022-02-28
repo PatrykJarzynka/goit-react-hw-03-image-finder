1d1f9dddcd146ce122ae2d1e6cdd56732d491a23
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2022-02-28 16:15:59 +0100

    Poprawiono wygląd guzika

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,19 @@
+import styled from '@emotion/styled';
+
+const FancyButton = styled.button({
+  backgroundColor: '#3f51b5',
+  color: 'white',
+  border: 'none',
+  padding: 10,
+  borderRadius: 5,
+  cursor: 'pointer',
+});
 
 function Button({ onClick }) {
   return (
-    <button type="button" onClick={onClick}>
+    <FancyButton type="button" onClick={onClick}>
       Load More
-    </button>
+    </FancyButton>
   );
 }
 
